8faee9e8bfa83e11e5a46f3195218c379f4a234c
martinyis <martinyis11@gmail.com>
2023-08-19 17:49:04 -0700

    Add reveal effect to experiances

--- components/About/ExpeirianceCard.tsx
+++ components/About/ExpeirianceCard.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import { TypeExpreiance } from "@/data/expriances";
-
+import Link from "next/link";
 const ExpeirianceCard = (props: TypeExpreiance) => {
-  const { date, role, company, description } = props;
+  const { date, role, company, description, companyLink } = props;
   return (
     <div className="p-[32px] bg-[#233554] rounded-[12px]">
       <p className="text-[20px] text-white">{date}</p>
       <h2 className="text-primary text-[26px] font-bold">{role}</h2>
-      <p className="text-[16px] text-white font-bold mb-[16px]">{company}</p>
+      <Link href={`${companyLink}`} target="_blank">
+        <p className="text-[16px] text-white font-bold mb-[16px] hover:text-primary">
+          {company}
+        </p>
+      </Link>
       <p className="text-[16px] text-[#D9D9D9]">{description}</p>
     </div>
   );
--- components/About/Experiances.tsx
+++ components/About/Experiances.tsx
@@ -1,6 +1,8 @@
+"use client";
 import React from "react";
 import { experiances } from "@/data/expriances";
 import ExpeirianceCard from "./ExpeirianceCard";
+import { Slide } from "react-awesome-reveal";
 type Props = {};
 
 const Experiances = (props: Props) => {
@@ -10,11 +12,13 @@ const Experiances = (props: Props) => {
         My <span className="text-primary">Experiences</span>
       </h2>
       <p className="text-[16px] text-[#9CA3AF] max-w-[730px] mb-[80px]"></p>
-      <div className="grid grid-cols-3 gap-[29px] md:grid-cols-2 sm:grid-cols-1">
-        {experiances.map((experiance, id) => (
-          <ExpeirianceCard {...experiance} key={id} />
-        ))}
-      </div>
+      <Slide direction="up" triggerOnce={true}>
+        <div className="grid grid-cols-3 gap-[29px] md:grid-cols-2 sm:grid-cols-1">
+          {experiances.map((experiance, id) => (
+            <ExpeirianceCard {...experiance} key={id} />
+          ))}
+        </div>
+      </Slide>
     </section>
   );
 };
