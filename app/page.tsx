dc0ab41bcc9c40f651a538ceca288a824d7e0a3e
Martin Babak <msb1093@unh.edu>
2023-12-22 18:45:41 -0800

    get rid of h1

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,6 @@ const images = [
 export default function Home() {
   return (
     <main className="max-w-[1400px] px-10 md:px-5 mx-auto pb-[109px]">
-      h1
       <About />
       <Experiances />
       <Projects />
